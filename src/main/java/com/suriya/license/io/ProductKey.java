a117bad9853b230ab8f5291011d22d565d8887ba
suriyaprakhash <suriyaprakhash@gmail.com>
2022-10-23 17:08:24 -0400

    licensemanger added

---
+++ src/main/java/com/suriya/license/io/ProductKey.java
@@ -0,0 +1,16 @@
+package com.suriya.license.io;
+
+import java.util.Map;
+
+public class ProductKey implements BaseProductKey {
+
+    private String filePath;
+    private String fileName;
+    private String filePassword;
+
+    private String productName;
+    private String keyPassword;
+
+    private Map<String, String> attributeMap;
+
+}
